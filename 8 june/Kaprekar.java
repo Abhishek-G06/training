c316d3c3e800e15dfc82c5d8e2cb3baa1a59c1da
Abhishek Gupta <abhi2002gupta@gmail.com>
2023-06-08 16:12:43 +0530

    new changes

--- 8 june/Kaprekar.java
+++ 8 june/Kaprekar.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Kaprekar
 {
-    public static void main()
+    public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number:");
